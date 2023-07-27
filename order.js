374ddd294302db1f9884f3b9879141da506cbb5e
vinayakgit1 <wadkarvinayak999@gmail.com>
2023-07-27 16:47:43 +0530

    addnew

---
+++ order.js
@@ -0,0 +1,40 @@
+const Orders=[
+{
+    productName:'Foldable Mini Drone',
+    ProductNumber:'85631',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'LARVENDER K102 Drone',
+    ProductNumber:'3678',
+    paymentStatus:'Refunded',
+    shipping:'Declined'
+},
+{
+    productName:'Ruko F11 Pro Drone',
+    ProductNumber:'49347',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'Drone With Camera Drone',
+    ProductNumber:'96996',
+    paymentStatus:'Paid',
+    shipping:'Deliverd'
+},
+{
+    productName:'Gps 4K Drone',
+    ProductNumber:'22821',
+    paymentStatus:'paid',
+    shipping:'Deliverd'
+},
+{
+    productName:'DJI Air 25',
+    ProductNumber:'00428',
+    paymentStatus:'paid',
+    shipping:'Deliverd'
+},
+
+]
+    
